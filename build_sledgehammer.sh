612c12b6b487c6a9269f2e97d6201dfff25e0a66
Victor Lowther <victor_lowther@dell.com>
2011-08-01 09:49:19 -0500

    Make build_sledgehammer.sh explain where to download the CentOS DVD image we need to stage the build.

--- build_sledgehammer.sh
+++ build_sledgehammer.sh
@@ -132,8 +132,24 @@ if ! which ruby &>/dev/null; then
     die "You must have Ruby installed to run this script.  We cannot proceed."
 fi
 
-[[ $CENTOS_ISO && -f $CENTOS_ISO ]] || \
+[[ $CENTOS_ISO && -f $CENTOS_ISO ]] || {
+    cat <<EOF
+You need to download the CentOS 5.6 x86_64 DVD in order to stage the
+Sledgehammer build.  You can download it via bittorrent using the following
+.torrent file:
+http://mirror.cs.vt.edu/pub/CentOS/5.6/isos/x86_64/CentOS-5.6-x86_64-bin-DVD.torrent
+
+If you cannot download the DVD using bittorrent, you can also download it via 
+direct download from:
+http://mirror.cs.vt.edu/pub/CentOS/5.6/isos/x86_64/CentOS-5.6-x86_64-bin-DVD-1of2.iso
+
+Once you have downloaded the CentOS 5.6 DVD isos, point CENTOS_ISO at the first
+ISO image and run build_sledgehammer.sh:
+CENTOS_ISO=/path/to/CentOS-5.6-x86_64-bin-DVD-1of2.iso ./build_sledgehammer.sh
+
+EOF
     die "You must have the Centos 5.6 install DVD downloaded, and CENTOS_ISO must point to it."
+}
 
 # Make a directory for chroots and to mount the ISO on.
 [[ $CHROOT ]] || CHROOT=$(mktemp -d "$HOME/.sledgehammer_chroot.XXXXX")
