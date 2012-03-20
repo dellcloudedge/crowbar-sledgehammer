43d19e920ec886793ad31ffb7f8a0429978f30a6
Victor Lowther <victor.lowther@gmail.com>
2012-03-20 16:06:09 -0500

    First pass at CentOS 6.2 support.

--- build_sledgehammer.sh
+++ build_sledgehammer.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# Copyright 2011, Dell 
-# 
-# Licensed under the Apache License, Version 2.0 (the "License"); 
-# you may not use this file except in compliance with the License. 
-# You may obtain a copy of the License at 
-# 
-#  http://www.apache.org/licenses/LICENSE-2.0 
-# 
-# Unless required by applicable law or agreed to in writing, software 
-# distributed under the License is distributed on an "AS IS" BASIS, 
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-# See the License for the specific language governing permissions and 
-# limitations under the License. 
-# 
+# Copyright 2011, Dell
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Author: VictorLowther
 #
 # If you are running this on a Redhat derived system, SELinux needs to be
@@ -42,7 +42,7 @@
 # Location for caches that should not be erased between runs
 [[ $CACHE_DIR ]] || CACHE_DIR="$HOME/.crowbar-build-cache"
 
-[[ $CENTOS_ISO ]] || CENTOS_ISO="$CACHE_DIR/iso/CentOS-5.7-x86_64-bin-DVD-1of2.iso"
+[[ $CENTOS_ISO ]] || CENTOS_ISO="$CACHE_DIR/iso/CentOS-6.2-x86_64-bin-DVD1.iso"
 
 die() { echo "$@"; exit 1; }
 shopt -s extglob
@@ -50,13 +50,12 @@ shopt -s extglob
 cleanup() {
     # Clean up any stray mounts we may have left behind.
     while read dev fs type opts rest; do
-	sudo umount "$fs"
+        sudo umount -l "$fs"
     done < <(tac /proc/self/mounts |grep "$CHROOT")
-    [[ $webrick_pid ]] && kill -9 $webrick_pid
     # use lazy unmount other wise dev won't unmount and the rm will
     # will trash the real dev mount.
     sudo umount -l "$BUILD_DIR"
-    sudo rm -rf "$BUILD_DIR" "$CHROOT" 
+    sudo rm -rf "$BUILD_DIR" "$CHROOT"
 }
 
 OS_BASIC_PACKAGES=(MAKEDEV SysVinit audit-libs basesystem bash beecrypt \
@@ -71,11 +70,12 @@ OS_BASIC_PACKAGES=(MAKEDEV SysVinit audit-libs basesystem bash beecrypt \
     readline rpm rpm-libs rpm-python sed setup shadow-utils sqlite sysklogd \
     termcap tzdata udev util-linux yum yum-metadata-parser zlib)
 
-EXTRA_REPOS=('http://mirror.centos.org/centos/5/updates/$basearch' \
-    'http://mirror.centos.org/centos/5/extras/$basearch' \
-    'http://mirror.pnl.gov/epel/5/$basearch' \
+EXTRA_REPOS=('http://mirror.centos.org/centos/6/os/$basearch' \
+    'http://mirror.centos.org/centos/6/updates/$basearch' \
+    'http://mirror.centos.org/centos/6/extras/$basearch' \
+    'http://mirror.pnl.gov/epel/6/$basearch' \
     'http://www.nanotechnologies.qc.ca/propos/linux/centos-live/$basearch/live' \
-    'http://rbel.frameos.org/stable/el5/$basearch')
+    'http://rbel.frameos.org/stable/el6/$basearch')
 
 
 in_chroot() { sudo -H /usr/sbin/chroot "$CHROOT" "$@"; }
@@ -85,53 +85,53 @@ chroot_fetch() { in_chroot /usr/bin/yum -y --downloadonly install "$@"; }
 make_redhat_chroot() (
     postcmds=()
     mkdir -p "$CHROOT"
-    cd "$BUILD_DIR/CentOS"
+    cd "$BUILD_DIR/Packages"
     # first, extract our core files into the chroot.
     for pkg in "${OS_BASIC_PACKAGES[@]}"; do
-	for f in "$pkg"-[0-9]*+(noarch|x86_64).rpm; do
-	    rpm2cpio "$f" | (cd "$CHROOT"; sudo cpio --extract \
-		--make-directories --no-absolute-filenames \
-		--preserve-modification-time)
-	done
-	if [[ $pkg =~ (centos|redhat)-release ]]; then
-	    mkdir -p "$CHROOT/tmp"
-	    cp "$f" "$CHROOT/tmp/$f"
-	    postcmds+=("/bin/rpm -ivh --force --nodeps /tmp/$f")
-	fi
+        for f in "$pkg"-[0-9]*+(noarch|x86_64).rpm; do
+            rpm2cpio "$f" | (cd "$CHROOT"; sudo cpio --extract \
+                --make-directories --no-absolute-filenames \
+                --preserve-modification-time)
+        done
+        if [[ $pkg =~ (centos|redhat)-release ]]; then
+            mkdir -p "$CHROOT/tmp"
+            cp "$f" "$CHROOT/tmp/$f"
+            postcmds+=("/bin/rpm -ivh --force --nodeps /tmp/$f")
+        fi
     done
     # second, fix up the chroot to make sure we can use it
     sudo cp /etc/resolv.conf "$CHROOT/etc/resolv.conf"
-    for d in proc sys dev dev/pts; do
-	mkdir -p "$CHROOT/$d"
-	sudo mount --bind "/$d" "$CHROOT/$d"
+    for d in /proc /sys /dev /dev/pts /dev/shm; do
+        [[ -L $d ]] && d="$(readlink -f "$d")"
+        mkdir -p "${CHROOT}$d"
+        sudo mount --bind "$d" "${CHROOT}$d"
     done
     # third, run any post cmds we got earlier
     for cmd in "${postcmds[@]}"; do
-	in_chroot $cmd
+        in_chroot $cmd
     done
-    repo=$(mktemp /tmp/crowbar-repo-XXXXXXXX)
-    cat >"$repo" <<EOF
-[redhat-base]
-name=Redhat Base Repo
-baseurl=http://127.0.0.1:54321/
-enabled=1
-gpgcheck=0
-EOF
     sudo rm -f "$CHROOT/etc/yum.repos.d/"*
-    sudo cp "$repo" "$CHROOT/etc/yum.repos.d/crowbar-build-base.repo"
 
-    # Work around packages we don't have, but that the yum bootstrap
-    # will grab for us.
-    in_chroot mkdir -p "/usr/lib/python2.4/site-packages/urlgrabber.broke"
-    for f in "$CHROOT/usr/lib/python2.4/site-packages/urlgrabber/keepalive"*; do
-	in_chroot mv "${f#$CHROOT}" \
-	    "/usr/lib/python2.4/site-packages/urlgrabber.broke/"
+    rnum=0
+    for repo in "${EXTRA_REPOS[@]}"; do
+        rt=$(mktemp "/tmp/r${rnum}-XXXXXXXX")
+        cat > "$rt".repo <<EOF
+[r${rnum}]
+name=Repo $rnum
+baseurl=$repo
+gpgcheck=0
+enabled=1
+EOF
+        rnum=$(($rnum + 1))
+        sudo cp "$rt".repo "$CHROOT/etc/yum.repos.d/"
+        rm -f "$rt".repo
     done
     # Make sure yum does not throw away our caches for any reason.
     in_chroot /bin/sed -i -e '/keepcache/ s/0/1/' /etc/yum.conf
     in_chroot sh -c "echo 'exclude = *.i386' >>/etc/yum.conf"
     # fourth, have yum bootstrap everything else into usefulness
     chroot_install yum yum-downloadonly
+    in_chroot ln -s /proc/self/mounts /etc/mtab
 )
 
 [[ -f ${0##*/} ]] || \
@@ -151,21 +151,21 @@ fi
 
 [[ $CENTOS_ISO && -f $CENTOS_ISO ]] || {
     cat <<EOF
-You need to download the CentOS 5.7 x86_64 DVD in order to stage the
+You need to download the CentOS 6.2 x86_64 DVD in order to stage the
 Sledgehammer build.  You can download it via bittorrent using the following
 .torrent file:
-http://mirror.cs.vt.edu/pub/CentOS/5.7/isos/x86_64/CentOS-5.7-x86_64-bin-DVD.torrent
+http://mirror.cs.vt.edu/pub/CentOS/6.2/isos/x86_64/CentOS-6.2-x86_64-bin-DVD1.torrent
 
-If you cannot download the DVD using bittorrent, you can also download it via 
+If you cannot download the DVD using bittorrent, you can also download it via
 direct download from:
-http://mirror.cs.vt.edu/pub/CentOS/5.7/isos/x86_64/CentOS-5.7-x86_64-bin-DVD-1of2.iso
+http://mirror.cs.vt.edu/pub/CentOS/6.2/isos/x86_64/CentOS-6.2-x86_64-bin-DVD1.iso"
 
-Once you have downloaded the CentOS 5.7 DVD isos, point CENTOS_ISO at the first
+Once you have downloaded the CentOS 6.2 DVD isos, point CENTOS_ISO at the first
 ISO image and run build_sledgehammer.sh:
-CENTOS_ISO=/path/to/CentOS-5.7-x86_64-bin-DVD-1of2.iso ./build_sledgehammer.sh
+CENTOS_ISO=/path/to/CentOS-6.2-x86_64-bin-DVD1.iso" ./build_sledgehammer.sh
 
 EOF
-    die "You must have the Centos 5.7 install DVD downloaded, and CENTOS_ISO must point to it."
+    die "You must have the Centos 6.2 install DVD downloaded, and CENTOS_ISO must point to it."
 }
 
 # Make a directory for chroots and to mount the ISO on.
@@ -173,30 +173,11 @@ EOF
 [[ $BUILD_DIR ]] || BUILD_DIR=$(mktemp -d "$HOME/.centos-image.XXXXXX")
 trap cleanup 0 INT QUIT TERM
 sudo mount -o loop "$CENTOS_ISO" "$BUILD_DIR"
-# Fire up Webrick to serve out the contents of the iso.
-(   cd "$BUILD_DIR"
-    exec ruby -rwebrick -e \
-	'WEBrick::HTTPServer.new(:BindAddress=>"127.0.0.1",:Port=>54321,:DocumentRoot=>".").start' ) &
-    webrick_pid=$!
 make_redhat_chroot
 
 # Put ourselves in /mnt in the chroot.
 sudo mount --bind "$PWD" "$CHROOT/mnt"
 # build our extra yum repositories.
-rnum=0
-for repo in "${EXTRA_REPOS[@]}"; do
-    rt=$(mktemp "/tmp/r${rnum}-XXXXXXXX")
-    cat > "$rt".repo <<EOF
-[r${rnum}]
-name=Repo $rnum
-baseurl=$repo
-gpgcheck=0
-enabled=1
-EOF
-    rnum=$(($rnum + 1))
-    sudo cp "$rt".repo "$CHROOT/etc/yum.repos.d/"
-    rm -f "$rt".repo
-done
 
 
 [[ $USE_PROXY = "1" ]] && (
@@ -217,25 +198,17 @@ done
 
 
 # Install the livecd tools and prerequisites.
-chroot_install livecd-tools livecd-installer rhpl kudzu
+chroot_install livecd-tools livecd-installer
 in_chroot /bin/mkdir -p /mnt/cache /mnt/bin
-# Force livecd-creator to use ext2 instead of ext3.
-in_chroot /bin/sed -i -e '/self.__fstype/ s/ext3/ext2/' \
-    /usr/lib/python2.4/site-packages/imgcreate/creator.py
-
-in_chroot /bin/mv "/sbin/mksquashfs" "/sbin/mksquashfs.orig"
-sudo cp "$PWD/buildenv/mksquashfs"  "$CHROOT/sbin"
-in_chroot /bin/chmod 777 /sbin/mksquashfs
-in_chroot /bin/chmod 777 /sbin/mksquashfs.orig
 
 # Regenerate the slectehammer.iso if it is not already there.
 if ! [[ -f $CHROOT/mnt/sledgehammer.iso ]]; then
     if [[ $USE_PROXY = "1" ]]; then
         in_chroot /bin/bash -c "cd /mnt; NO_PROXY=127.0.0.1 HTTP_PROXY=$PROXY_HOST:$PROXY_PORT /usr/bin/livecd-creator --config=centos-sledgehammer.ks --cache=./cache -f sledgehammer" || \
-    	die "Could not build full iso image"
+        die "Could not build full iso image"
     else
         in_chroot /bin/bash -c 'cd /mnt; /usr/bin/livecd-creator --config=centos-sledgehammer.ks --cache=./cache -f sledgehammer' || \
-    	die "Could not build full iso image"
+        die "Could not build full iso image"
     fi
 fi
 
--- centos-sledgehammer.ks
+++ centos-sledgehammer.ks
@@ -5,14 +5,15 @@ auth --useshadow --enablemd5
 selinux --disabled
 firewall --disabled
 
-repo --name=a-base    --baseurl=http://127.0.0.1:54321/
-repo --name=a-updates --baseurl=http://mirror.centos.org/centos/5/updates/$basearch
-repo --name=a-extras  --baseurl=http://mirror.centos.org/centos/5/extras/$basearch
-repo --name=a-epel    --baseurl=http://mirror.pnl.gov/epel/5/$basearch
+repo --name=a-base    --baseurl=http://mirror.centos.org/centos/6/os/$basearch
+repo --name=a-updates --baseurl=http://mirror.centos.org/centos/6/updates/$basearch
+repo --name=a-extras  --baseurl=http://mirror.centos.org/centos/6/extras/$basearch
+repo --name=a-epel    --baseurl=http://mirror.pnl.gov/epel/6/$basearch
 repo --name=a-live    --baseurl=http://www.nanotechnologies.qc.ca/propos/linux/centos-live/$basearch/live
-repo --name=a-rbel    --baseurl=http://rbel.frameos.org/stable/el5/$basearch
+repo --name=a-rbel    --baseurl=http://rbel.frameos.org/stable/el6/$basearch
 
 %packages
+@core
 bash
 kernel
 syslinux
@@ -36,7 +37,6 @@ procmail
 wget
 lshw
 dmidecode
-glibc.i686
 libxml2
 libxml2-devel
 zlib
@@ -53,7 +53,6 @@ tar
 gzip
 mktemp
 libsysfs.x86_64
-libsysfs.i386 
 yum
 curl
 ruby
@@ -127,3 +126,4 @@ EOF_postnochroot
 
 /bin/bash -x /root/postnochroot-install 2>&1 | tee /root/postnochroot-install.log
 
+%end
